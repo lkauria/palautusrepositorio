5d5630e93a8460739ee178c8f60b0491815eff70
Laura Kauria <laura.kauria@helsinki.fi>
2022-11-21 16:06:31 +0200

    print players in a better format

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,26 +1,20 @@
+from time import time
 import requests
 from player import Player
+from datetime import datetime
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
     players = []
 
     for player_dict in response:
-        if player_dict['nationality'] == "FIN":
-            player = Player(
-                player_dict['name'] + 
-                "\t team " + player_dict['nationality'] + 
-                "\t goals " +  str(player_dict['goals'])
-            )
+       
+        player = Player(player_dict['name'], player_dict['team'], player_dict['nationality'], player_dict['goals'], player_dict['assists'])
+        players.append(player)
 
-            players.append(player)
-
-    print("Oliot:")
+    print("Players from FIN ", datetime.now(), "\n") 
 
     for player in players:
         print(player)
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,10 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, team, nationality, goals, assists):
         self.name = name
+        self.nationlity = nationality
+        self.goals = goals
+        self.assists = assists
+        self.team = team
     
     def __str__(self):
-        return self.name
+        return f'{self.name:20} {self.team:3} {self.goals:2} + {self.assists:2} = {(self.goals + self.assists):3}'
