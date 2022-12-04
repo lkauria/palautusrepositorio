3c5936ef642eeadf61a4174b3fb7ae3fcd8060f9
Laura Kauria <laura.kauria@helsinki.fi>
2022-12-04 20:35:59 +0200

    partly refactored

--- viikko5/int-joukko/src/int_joukko.py
+++ viikko5/int-joukko/src/int_joukko.py
@@ -4,55 +4,27 @@ OLETUSKASVATUS = 5
 
 class IntJoukko:
     def __init__(self, kapasiteetti=None, kasvatuskoko=None):
-        if kapasiteetti is None:
-            self.kapasiteetti = KAPASITEETTI
-        elif not isinstance(kapasiteetti, int) or kapasiteetti < 0:
-            raise Exception("Väärä kapasiteetti")  # heitin vaan jotain :D
-        else:
-            self.kapasiteetti = kapasiteetti
-
-        if kasvatuskoko is None:
-            self.kasvatuskoko = OLETUSKASVATUS
-        elif not isinstance(kapasiteetti, int) or kapasiteetti < 0:
-            raise Exception("kapasiteetti2")  # heitin vaan jotain :D
-        else:
-            self.kasvatuskoko = kasvatuskoko
-
+        self.kapasiteetti = KAPASITEETTI
+        self.kasvatuskoko = OLETUSKASVATUS
+ 
         self.ljono = [0] * self.kapasiteetti
-
+        print(self.ljono)
         self.alkioiden_lkm = 0
 
     def kuuluu(self, n):
-        on = 0
-
-        for i in range(0, self.alkioiden_lkm):
+        for i in range(self.alkioiden_lkm):
             if n == self.ljono[i]:
-                on = on + 1
+                return True
 
-        if on > 0:
-            return True
-        else:
-            return False
+        return False
 
     def lisaa(self, n):
-        ei_ole = 0
-
-        if self.alkioiden_lkm == 0:
-            self.ljono[0] = n
-            self.alkioiden_lkm = self.alkioiden_lkm + 1
-            return True
-        else:
-            pass
-
-        if not self.kuuluu(n):
-            self.ljono[self.alkioiden_lkm] = n
+        if not self.kuuluu(n):          
+            self.ljono[self.alkioiden_lkm] = n  
             self.alkioiden_lkm = self.alkioiden_lkm + 1
 
-            if self.alkioiden_lkm % len(self.ljono) == 0:
-                taulukko_old = self.ljono
-                self.kopioi_taulukko(self.ljono, taulukko_old)
-                self.ljono = [0] * (self.alkioiden_lkm + self.kasvatuskoko)
-                self.kopioi_taulukko(taulukko_old, self.ljono)
+            if self.alkioiden_lkm % len(self.ljono) == 0: 
+                self.ljono = [0] * (self.alkioiden_lkm + self.kasvatuskoko)  
 
             return True
 
@@ -80,7 +52,7 @@ class IntJoukko:
         return False
 
     def kopioi_taulukko(self, a, b):
-        for i in range(0, len(a)):
+        for i in range(len(a)):
             b[i] = a[i]
 
     def mahtavuus(self):
